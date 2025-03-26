2d15eda0a79660af04261a3ed17d8b8a004a3cdd
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-26 14:10:47 -0400

    created multiplyByRegReuse()

--- l9t1.c
+++ l9t1.c
@@ -10,21 +10,44 @@ with two version of matrix multiplication program, compare their output.
 #include <time.h>
 #include <math.h>
 
-// normal matrix multiplication
-void multiplyMatrices(int size, double **A, double **B, double **C) {
+// set matrix C to 0
+void setMatrixProduct(int size, double *C) {
     // set product matrix C to 0
     for (int i = 0; i < size; i ++) {
         for (int j = 0; j < size; j++) {
-            C[i][j] = 0.0;
+            C[i] = 0.0;
         }
     }
+}
+
+// normal matrix multiplication
+void multiplyMatrices(int size, double *A, double *B, double *C) {
+    // set product to 0
+    setMatrixProduct(size, C);
 
     // multiply ijk
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             for (int k = 0; k < size; k++) {
-                C[i][j] = A[i][k] * B[k][j];
+                C[i * size + j] = A[i * size + k] * B[k * size + j];
             }
+        }
+    }
+}
+
+// register reuse multiplication
+void multiplyByRegReuse(int size, double *A, double *B, double *C) {
+    // set product to 0
+    setMatrixProduct(size, C);
+
+    // multiply ijk
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            register double sum = 0.0;
+            for (int k = 0; k < size; k++) {
+                sum += A[i * size + k] * B[k * size + j];
+            }
+            C[i * size + j] = sum;
         }
     }
 }

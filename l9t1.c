cf4b9b202d67edb933d7a4feb2ec083dae12bbc2
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-26 14:31:17 -0400

    changed to single dimension array

--- l9t1.c
+++ l9t1.c
@@ -29,7 +29,7 @@ void multiplyMatrices(int size, double *A, double *B, double *C) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             for (int k = 0; k < size; k++) {
-                C[i * size + j] = A[i * size + k] * B[k * size + j];
+                C[i * size + j] += A[i * size + k] * B[k * size + j];
             }
         }
     }
